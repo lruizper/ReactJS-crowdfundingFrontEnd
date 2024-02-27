89c33cc5df44f6350e274990b303498167e17fe2
lruizper <lruizper@gmail.com>
2024-02-27 13:15:32 +0800

    logOut button functioning

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,11 @@ import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
+
+    const handleLogout =()=> {
+        window.localStorage.removeItem("token");
+    }
+
     return (
         <div className="my-navBar">
             <nav>
@@ -9,8 +14,9 @@ function NavBar() {
                     <li><Link className="navButton" to="/">Home</Link></li>
                     <li><Link className="navButton" to="/about">About</Link></li>
                     <li><Link className="navButton" to="/contact">Contact</Link></li>
-                    <li><Link className="navButton" to="/login"> LogIn</Link></li>
-                    <li><Link className="navButton" to="/signin"> SignUp</Link></li>
+                    <li><Link className="navButton" to="/login">LogIn</Link></li>
+                    <li><Link className="navButton" to="/" onClick={handleLogout}>LogOut</Link></li>
+                    <li><Link className="navButton" to="/signin">Register</Link></li>
                     <li><Link className="navButton" to="/createproject"> Create Project</Link></li>
                 </ul>
             </nav>
