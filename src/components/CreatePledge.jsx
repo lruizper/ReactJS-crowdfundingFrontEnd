8e84181b468f22c8d8cf75a8e6a669c5c30f049c
lruizper <lruizper@gmail.com>
2024-03-02 21:24:25 +0800

    Pledges  component showing appropiately in the project page

--- src/components/CreatePledge.jsx
+++ src/components/CreatePledge.jsx
@@ -1,19 +1,23 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "./AuthProvider";
+import { useParams } from "react-router-dom";
+import useProject from "../hooks/use-project";
 import postPledge from "../api/post-pledge";
 
 function CreatePledge() {
     const navigate = useNavigate();
+    const { id } = useParams();
     const [newPledge, setNewPledge] = useState(
         {
-            "supporter": user_id, // need grab this from local storage
-            "project": project_id, // need grab this from current project page
+            "supporter": AuthContext.user_id, // need grab this from local storage
+            "project": id, // need grab this from current project page
             "ammount": "",
             "comment": "",
             "annonymous": "false",
         }
     );
-
+    console.log(newPledge)
     const handleChange = (event) => {
         const { id, value } = event.target;
         setNewProj((prevPledge) => ({
